3d6999a8c879f1d1d33f19d6a50aef0fbfc4a479
Mickey374 <asantemichael37483@gmail.com>
2023-09-26 02:50:09 +0200

    Added design for Navbar with additional components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Homebnb',
-  description: 'Airbnb Clone',
-}
+  title: "Homebnb",
+  description: "Airbnb Clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      
+      </body>
     </html>
-  )
+  );
 }
