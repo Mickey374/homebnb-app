f9101b3ce326461b0baeb2f2cce9ecbc82905ad8
Mickey374 <asantemichael37483@gmail.com>
2023-09-27 23:39:50 +0200

    Formatted Modal and added button component.

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect, useState, useCallback } from "react";
-import {IoMdClose} from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
   isOpen: boolean;
@@ -72,11 +73,26 @@ const Modal: React.FC<ModalProps> = ({
             <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
               {/* header */}
               <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
-                <button onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute left-9">
-                  <IoMdClose size={18}/>
+                <button
+                  onClick={handleClose}
+                  className="p-1 border-0 hover:opacity-70 transition absolute left-9"
+                >
+                  <IoMdClose size={18} />
                 </button>
+                <div className="text-lg font-semibold">{title}</div>
+              </div>
+              {/*Body*/}
+              <div className="relative p-6 flex-auto">
+                {body}
+              </div>
+              {/*Footer*/}
+              <div className="flex flex-col gap-2 p-6">
+                <div className="flex flex-row items-center gap-4 w-full">
+                  <Button label="My Btn"/>
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
