ba4b9c1afdadca06b049e4c494d819da2a028c6c
Mickey374 <asantemichael37483@gmail.com>
2023-09-29 02:42:05 +0200

    Completed registration form with Github and Google Signups.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import toast from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -43,14 +44,14 @@ const RegisterModal = () => {
       });
   };
 
-  const bodyContent  = (
+  const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading 
-      title="Welcome to Homebnb"
-      subtitle="Create an account."
-      center
+      <Heading
+        title="Welcome to Homebnb"
+        subtitle="Create an account."
+        center
       />
-      <Input 
+      <Input
         id="email"
         label="Email"
         disabled={isLoading}
@@ -58,7 +59,7 @@ const RegisterModal = () => {
         errors={errors}
         required
       />
-      <Input 
+      <Input
         id="name"
         label="Name"
         disabled={isLoading}
@@ -66,7 +67,7 @@ const RegisterModal = () => {
         errors={errors}
         required
       />
-      <Input 
+      <Input
         id="password"
         type="password"
         label="Password"
@@ -76,7 +77,31 @@ const RegisterModal = () => {
         required
       />
     </div>
-  )
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">Log in</div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <Modal
@@ -87,6 +112,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
