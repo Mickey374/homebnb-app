c87a4b04b03bd612a7bb5507dac9bb00e1bb1231
Mickey374 <asantemichael37483@gmail.com>
2023-09-30 14:02:54 +0200

    Generated action to fetch user info after a successful authentication.

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+
+// Get the session from the server
+export async function getSession() {
+    return await getServerSession(authOptions);
+}
+
+// Get the current user from the session
+export default async function getCurrentUser(){
+    try {
+        const session = await getSession();
+
+        if (!session?.user?.email) {
+            return null;
+        }
+
+        // Get the current user
+        const currentUser = await prisma.user.findUnique({
+            where: {
+                email: session.user.email 
+            }
+        });
+
+        //If the user doesn't exist
+        if(!currentUser) {
+            return null;
+        }
+
+        return currentUser;
+    } catch (error) {
+        return null;
+    }
+}
