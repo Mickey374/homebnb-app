ea9654947ff4fb118ba610703e9e7b64ce67fa1a
Mickey374 <asantemichael37483@gmail.com>
2023-10-07 01:50:18 +0200

    Added  Loading page to show when a page is loading

---
+++ app/components/Loader.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div className="h-[70vh] flex flex-col justify-center items-center">
+      <BounceLoader size={100} color="violet" />
+    </div>
+  );
+};
+
+export default Loader;
