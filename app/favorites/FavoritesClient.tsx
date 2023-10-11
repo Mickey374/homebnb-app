ed5367207e7104d5c31d90b876fa0c1856f1dcac
Mickey374 <asantemichael37483@gmail.com>
2023-10-11 22:04:00 +0200

    Reformatted the Listing card to include currentUser prop

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -7,7 +7,7 @@ import { SafeListing, SafeUser } from "../types";
 
 interface FavoritesClientProps {
   listings: SafeListing[];
-  currentUser?: SafeUser | null;
+  currentUser: SafeUser | null;
 }
 const FavoritesClient: React.FC<FavoritesClientProps> = ({
   listings,
@@ -22,7 +22,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
 
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 l:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {listings.map((listing) => (
-          <ListingCard key={listing.id} data={listing} />
+          <ListingCard key={listing.id} data={listing} currentUser={currentUser}/>
         ))}
       </div>
     </Container>
